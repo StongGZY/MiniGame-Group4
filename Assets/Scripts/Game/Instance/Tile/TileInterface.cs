ebbe57f8849166a126adb0a3fbb2bd4f80b4cb73
E75-ts <740688167@qq.com>
2022-05-28 11:37:07 +0800

    单位接口

---
+++ Assets/Scripts/Game/Instance/Tile/TileInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Landform
+
+
+interface TileInterface
+{
+	int GetYield(int yieldIndex);
+
+	int GetYieldReserve(int yieldIndex);
+
+	void ReduceYieldReserve(int yieldIndex, int amount);
+
+	int getTileType();
+
+}
